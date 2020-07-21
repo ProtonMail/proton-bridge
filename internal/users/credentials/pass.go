6023162443941c084a4b2144705b0236c466fb6d
James Houlahan <james.houlahan@protonmail.com>
2020-07-21 13:34:20 +0200

    feat: add build tag for deterministic imap password

--- internal/users/credentials/pass.go
+++ internal/users/credentials/pass.go
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with ProtonMail Bridge.  If not, see <https://www.gnu.org/licenses/>.
 
+// +build !imaptest
+
 package credentials
 
 import (
