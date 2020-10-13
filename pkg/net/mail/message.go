454fcd8b13b1f10e1512e5f800eec34f32e84910
Radomír Polách <rp@t4d.cz>
2020-10-13 17:33:53 +0200

    feature: improve net/mail date parsing

--- pkg/net/mail/message.go
+++ pkg/net/mail/message.go
@@ -74,18 +74,19 @@ var (
 func buildDateLayouts() {
 	// Generate layouts based on RFC 5322, section 3.3.
 
-	dows := [...]string{"", "Mon, "}   // day-of-week
+	dows := [...]string{"Mon, ", ""}   // day-of-week
 	days := [...]string{"2", "02"}     // day = 1*2DIGIT
 	years := [...]string{"2006", "06"} // year = 4*DIGIT / 2*DIGIT
 	seconds := [...]string{":05", ""}  // second
 	// "-0700 (MST)" is not in RFC 5322, but is common.
-	zones := [...]string{"-0700", "MST"} // zone = (("+" / "-") 4DIGIT) / "GMT" / ...
+	// Note: This is acutally a comment, added more relaxed timezones.
+	zones := [...]string{"-0700", "MST", "", "GMT-0700"} // zone = (("+" / "-") 4DIGIT) / "GMT" / ...
 
 	for _, dow := range dows {
-		for _, day := range days {
-			for _, year := range years {
-				for _, second := range seconds {
-					for _, zone := range zones {
+		for _, second := range seconds {
+			for _, zone := range zones {
+				for _, year := range years {
+					for _, day := range days {
 						s := dow + day + " Jan " + year + " 15:04" + second + " " + zone
 						dateLayouts = append(dateLayouts, s)
 					}
