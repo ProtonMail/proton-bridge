a62fa132e65ce00d9822303f0c79d9c7268dfaf2
Jakub <jakub@cuth.sk>
2020-11-06 16:02:30 +0100

    rename build tag

--- pkg/pmapi/config_qa.go
+++ pkg/pmapi/config_qa.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with ProtonMail Bridge.  If not, see <https://www.gnu.org/licenses/>.
 
-// +build pmapi_env
+// +build pmapi_qa
 
 package pmapi
 
