2a078b76e6ec4ec9aeee6eab491e47832046e349
James Houlahan <james.houlahan@protonmail.com>
2021-02-15 14:32:09 +0100

    GODT-1045 build without Qt by default

--- internal/frontend/qt-ie/enums.go
+++ internal/frontend/qt-ie/enums.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with ProtonMail Bridge.  If not, see <https://www.gnu.org/licenses/>.
 
-// +build !nogui
+// +build build_qt
 
 package qtie
 
