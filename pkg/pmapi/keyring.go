b2e9c4e4e98c5a6a7a71911085acfe1aa2b5fa66
James Houlahan <james.houlahan@protonmail.com>
2021-07-08 12:54:13 +0200

    Other: Revert "GODT-1224: don't strip trailing newlines from message bodies"
    This reverts commit 54161e263fa2f95795fc8623e9dfd2134afb0ae5.

--- pkg/pmapi/keyring.go
+++ pkg/pmapi/keyring.go
@@ -325,7 +325,7 @@ func encryptSymmDecryptKey(
 		return
 	}
 
-	pgpMessage, err := firstKey.Encrypt(crypto.NewPlainMessage([]byte(textToEncrypt)), kr)
+	pgpMessage, err := firstKey.Encrypt(crypto.NewPlainMessageFromString(textToEncrypt), kr)
 	if err != nil {
 		return
 	}
