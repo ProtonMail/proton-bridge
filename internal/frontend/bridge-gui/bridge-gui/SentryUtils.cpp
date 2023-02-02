5ec1da34b4bc8d1570b3c2661fa6f69b2895c323
Romain Le Jeune <romain.le.jeune@proton.ch>
2023-02-02 15:36:37 +0000

    feat(GODT-2278): improve sentry logs.

--- internal/frontend/bridge-gui/bridge-gui/SentryUtils.cpp
+++ internal/frontend/bridge-gui/bridge-gui/SentryUtils.cpp
@@ -16,19 +16,38 @@
 // along with Proton Mail Bridge. If not, see <https://www.gnu.org/licenses/>.
 
 #include "SentryUtils.h"
+#include "Version.h"
+#include <bridgepp/BridgeUtils.h>
 
+#include <QByteArray>
+#include <QCryptographicHash>
+#include <QString>
+#include <QSysInfo>
 
 static constexpr const char *LoggerName = "bridge-gui";
 
+QByteArray getProtectedHostname() {
+    QByteArray hostname = QCryptographicHash::hash(QSysInfo::machineHostName().toUtf8(), QCryptographicHash::Sha256);
+    return hostname.toHex();
+}
 
-void reportSentryEvent(sentry_level_t level, const char *message) {
+void setSentryReportScope() {
+    sentry_set_tag("OS", bridgepp::goos().toUtf8());
+    sentry_set_tag("Client", PROJECT_FULL_NAME);
+    sentry_set_tag("Version",   PROJECT_VER);
+    sentry_set_tag("UserAgent", QString("/ (%1)").arg(bridgepp::goos()).toUtf8());
+    sentry_set_tag("HostArch",  QSysInfo::currentCpuArchitecture().toUtf8());
+    sentry_set_tag("server_name",  getProtectedHostname());
+}
+
+sentry_uuid_t reportSentryEvent(sentry_level_t level, const char *message) {
     auto event = sentry_value_new_message_event(level, LoggerName, message);
-    sentry_capture_event(event);
+    return sentry_capture_event(event);
 }
 
 
-void reportSentryException(sentry_level_t level, const char *message, const char *exceptionType, const char *exception) {
+sentry_uuid_t reportSentryException(sentry_level_t level, const char *message, const char *exceptionType, const char *exception) {
     auto event = sentry_value_new_message_event(level, LoggerName, message);
     sentry_event_add_exception(event, sentry_value_new_exception(exceptionType, exception));
-    sentry_capture_event(event);
+    return sentry_capture_event(event);
 }
