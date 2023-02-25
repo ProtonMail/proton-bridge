4a6460f3ed8f4425762899c6fea324c3eaf12a40
Mark Conway <mark.conway@themobiusproject.com>
2023-02-25 12:36:11 -0600

    feat(GODT-2411): allow qmake executable to be named qmake6.
    And a free a typo fix.
    
    (cherry picked from commit 4153a6925321e89e3ace7d747109484c1d28162e)

--- internal/frontend/bridge-gui/FindQt.cmake
+++ internal/frontend/bridge-gui/FindQt.cmake
@@ -1,6 +1,6 @@
-find_program(QMAKE_EXE "qmake")
+find_program(QMAKE_EXE NAMES "qmake" "qmake6")
 if (NOT QMAKE_EXE)
-    message(FATAL_ERROR "Could not locate qmake executable, make sur you have Qt 6 installed in that qmake is in your PATH environment variable.")
+    message(FATAL_ERROR "Could not locate qmake executable, make sure you have Qt 6 installed in that qmake is in your PATH environment variable.")
 endif()
 message(STATUS "Found qmake at ${QMAKE_EXE}")
 execute_process(COMMAND "${QMAKE_EXE}" -query QT_INSTALL_PREFIX OUTPUT_VARIABLE QT_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
