58b45d8458832bf5efd8fdf5c6b9c13f3f69f22c
Xavier Michelon <xavier@michelon.ch>
2023-06-27 17:08:09 +0200

    feat(GODT-2728): remove the sentry report for gRPC event stream interruptions in bridge-gui.

--- internal/frontend/bridge-gui/bridge-gui/EventStreamWorker.cpp
+++ internal/frontend/bridge-gui/bridge-gui/EventStreamWorker.cpp
@@ -43,12 +43,9 @@ EventStreamReader::EventStreamReader(QObject *parent)
 void EventStreamReader::run() {
     try {
         emit started();
-
-        grpc::Status const status = app().grpc().runEventStreamReader();
-        if (!status.ok()) {
-            throw Exception(QString::fromStdString(status.error_message()));
-        }
-
+        // Status code for the call below is ignored. The event stream may have interrupted by system shutdown or OS user sign-out, and we do not
+        // want this to generate a sentry report.
+        app().grpc().runEventStreamReader();
         emit finished();
     }
     catch (Exception const &e) {
