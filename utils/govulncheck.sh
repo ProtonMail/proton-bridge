fe39d23cf8c29f328d090222ada1c0b67f7273db
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2025-02-10 12:53:07 +0100

    chore(BRIDGE-315): silence crypto/internal/nistec vuln

--- utils/govulncheck.sh
+++ utils/govulncheck.sh
@@ -30,6 +30,7 @@ main(){
     ignore GO-2023-2328 "GODT-3124 RESTY race condition"
     ignore GO-2025-3373 "BRIDGE-315 stdlib crypto/x509"
     ignore GO-2025-3420 "BRIDGE-315 stdlib net/http"
+    ignore GO-2025-3447 "BRIDGE-315 stdlib crypto/internal/nistec"
 
     has_vulns
 
